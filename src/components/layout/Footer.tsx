a35314f3e1276e2942919666ee4dee5a8a5dcb1f
Baptiste-Gfy <baptistegeffroy@bgs-mbp.home>
2024-04-21 23:49:07 +0200

    add a minimal Layout for try

---
+++ src/components/layout/Footer.tsx
@@ -0,0 +1,7 @@
+export const Footer = () => {
+  return (
+    <footer className="flex items-center justify-center border-t border-border p-2">
+      <p>I&apos;m a Footer</p>
+    </footer>
+  );
+};
