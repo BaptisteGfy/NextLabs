f4211bb091c740603513ad29d9aa7b262b0a78a7
Baptiste-Gfy <baptistegeffroy@bgs-mbp.home>
2024-04-26 04:22:53 +0200

    [Update] - navigation added in header

--- app/games/page.tsx
+++ app/games/page.tsx
@@ -1,8 +1,10 @@
-export default function Home() {
+import React from "react";
+
+export default function Gamespage() {
   return (
     <main className="flex flex-1 flex-col items-center justify-center gap-10">
       <h1>Hello World 👋</h1>
-      <p>Welcom</p>
+      <p>Ici il y aura des jeux ( tetris... )</p>
     </main>
   );
 }
--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,14 +1,37 @@
 import { ThemeToggle } from "@/components/ThemeToggle";
+import Link from "next/link";
+import { HomeIcon } from "lucide-react";
 
 export const Header = () => {
   return (
-    <>
-      <header className="relative flex items-center justify-center border-b border-border p-2">
-        <p className="text-center">I&apos;m a Header</p>
-        <span className="absolute right-2">
-          <ThemeToggle />
-        </span>
-      </header>
-    </>
+    <header className="sticky top-0 z-40 flex w-full items-center border-b bg-background">
+      {/* Logo */}
+      <span className="mx-5 my-2 flex items-center justify-around gap-2 rounded-sm p-2">
+        <div className="flex size-10 items-center justify-center rounded-full bg-gradient-to-br from-purple-900 to-blue-900  ">
+          <Link href="/">
+            <HomeIcon size={25} absoluteStrokeWidth />
+          </Link>
+        </div>
+      </span>
+      {/* Nav */}
+      <span className="flex justify-around gap-5">
+        <Link
+          className="text-muted-foreground hover:text-foreground hover:underline"
+          href="/blog"
+        >
+          Blog
+        </Link>
+        <Link
+          className="text-muted-foreground hover:text-foreground hover:underline"
+          href="/games"
+        >
+          Games
+        </Link>
+      </span>
+      {/* Theme Toggle */}
+      <span className="ml-auto mr-5">
+        <ThemeToggle />
+      </span>
+    </header>
   );
 };
